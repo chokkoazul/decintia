78fc3679cd66e47b072720a12ff7e87b9376c778
Cristian Osorio <cristian.osorio@globant.com>
2020-11-09 00:14:14 -0300

    se agrega estructura para vercel

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('*', (req, res) => {
+    res.send({ mensaje: "Todo ok"})
+}
+);
+
+app.listen(3000, () => {
+    console.log(`Server is listening on port 3000`);
+  });
